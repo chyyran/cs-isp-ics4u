135d1d75ebaa07bccd03544c5686ff382e653971
RonnChyran <ronny6993@gmail.com>
2017-01-20 17:49:46 -0500

    add errors

--- compile and run.bat
+++ compile and run.bat
@@ -3,6 +3,9 @@ set PATH=%PATH%;"F:\Program Files\Java\jdk8\bin";"%PROGRAMFILES%/Java/jdk8/bin"
 echo Building...
 mkdir out
 javac src/pokemon/menu/MainMenu.java -cp src -d out
+echo press any key to run
+pause
 echo Running...
 cls
 java -cp out/ pokemon.menu.MainMenu 
+pause
