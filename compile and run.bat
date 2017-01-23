851c972b761ef093cf406acdf098a0f601fbd128
RonnChyran <ronny6993@gmail.com>
2017-01-22 19:32:42 -0500

    cleanup

--- compile and run.bat
+++ compile and run.bat
@@ -1,10 +1,11 @@
 @echo off
 set PATH=%PATH%;"F:\Program Files\Java\jdk8\bin";"%PROGRAMFILES%/Java/jdk1.8.0_121/bin"
 echo Building...
+rmdir out /S /Q
 mkdir out
 javac src/pokemon/menu/MainMenu.java -cp src -d out
-echo press any key to run
-pause
+echo Press any key to run
+pause >nul
 echo Running...
 cls
 java -cp out/ pokemon.menu.MainMenu 
